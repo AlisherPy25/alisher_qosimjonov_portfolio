2c2b9214d65113a8aa76efe6f9567d057bb9a95e
AlisherPy25 <167871776+AlisherPy25@users.noreply.github.com>
2025-04-12 02:02:45 +0500

    ВА

--- src/assets/index.js
+++ src/assets/index.js
@@ -28,7 +28,39 @@ import {
   tailwind,
   threejs
 } from '../assets';
+// Основные иконки
+export { default as logo } from './logo.png';
+export { default as backend } from './backend.png';
+export { default as creator } from './creator.png';
+export { default as mobile } from './mobile.png';
+export { default as web } from './web.png';
+export { default as github } from './github.png';
+export { default as menu } from './menu.svg';
+export { default as close } from './close.svg';
 
+// Технологии
+export { default as css } from './css.png';
+export { default as git } from './git.png';
+export { default as html } from './html.png';
+export { default as javascript } from './javascript.png';
+export { default as mongodb } from './mongodb.png';
+export { default as nodejs } from './nodejs.png';
+export { default as reactjs } from './reactjs.png';
+export { default as redux } from './redux.png';
+export { default as tailwind } from './tailwind.png';
+export { default as threejs } from './threejs.png';
+export { default as gsap } from './gsap.png';
+export { default as framer } from './framer.png';
+export { default as figma } from './figma.png';
+export { default as mysql } from './mysql.png';
+export { default as express } from './express.png';
+export { default as aws } from './aws.png';
+export { default as mui } from './mui.png';
+
+// Проекты
+export { default as gearXpert } from './gearXpert.png';
+export { default as project2 } from './project2.png';
+export { default as project3 } from './project3.png';
 export const navLinks = [
   {
     id: "about",
