ca9070cd757f8b577f7088cd86a45021b21c2558
AlisherPy25 <167871776+AlisherPy25@users.noreply.github.com>
2025-04-12 01:50:36 +0500

    fulll

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,36 +1,4 @@
-import logo from "./logo.png";
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
-import github from "./github.png";
-import menu from "./menu.svg";
-import close from "./close.svg";
-import gsap from './tech/gsap.png'
-import framer from './tech/framer.png';
-import css from "./tech/css.png";
-import figma from "./tech/figma.png";
-import gearXpert from './gearXpert.png';
-import project3 from './project3.webp';
-import project2 from './proj1.webp'
-import mysql from './tech/mysql.png';
-import express from './tech/express.png';
-import aws from './tech/aws.png';
-import mui from './tech/mui.png'
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import threejs from "./tech/threejs.svg";
-import firstTestimonial from './Tars Ken.webp';
-import secondTestimonial from './image.webp';
-import thirdTestimonial from './third testimonial.webp'
-
-export {
+import {
   logo,
   backend,
   creator,
@@ -47,9 +15,6 @@ export {
   express,
   aws,
   mui,
-  firstTestimonial,
-  secondTestimonial,
-  thirdTestimonial,
   gsap,
   framer,
   figma,
@@ -61,6 +26,91 @@ export {
   reactjs,
   redux,
   tailwind,
-  threejs,
+  threejs
+} from '../assets';
 
-};
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Projects", 
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Pentester",
+    icon: web,
+  },
+  {
+    title: "Frontend Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Ethical Hacker",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "GSAP",
+    icon: gsap,
+  },
+  {
+    name: "Framer Motion",
+    icon: framer,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "Git",
+    icon: git,
+  }
+];
+
+const projects = [
+  {
+    name: "My Project",
+    description: "This is my first project",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      }
+    ],
+    image: project2,
+    source_code_link: "https://github.com/",
+  }
+];
+
+export { services, technologies, projects };
