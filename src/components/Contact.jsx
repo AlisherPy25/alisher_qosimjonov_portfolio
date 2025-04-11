93e43640077d1bbcb829ba11e6ff8863a73ac105
AlisherPy25 <167871776+AlisherPy25@users.noreply.github.com>
2025-04-12 01:33:27 +0500

    alishers_portfolio_complate

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Alisher Qosimjonov,
+          to_name: "Alisher Qosimjonov",
           from_email: form.email,
           to_email: "alishka4334@gmail.com",
           message: form.message,
